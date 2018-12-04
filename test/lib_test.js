3c4b9680454e4aec9b2fcd3bdbac1ea64430b070
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-04 18:34:22 +0530

    added more test cases in test file

--- test/lib_test.js
+++ test/lib_test.js
@@ -2,31 +2,70 @@ const assert = require('assert');
 const {createHeading, extractLines, extractBytes, findInteger} = require('../src/lib.js');
 
 describe('createHeading', function(){
+
   it('should create a head line using a file name', function(){
     assert.equal(createHeading('lib.js'),'==> lib.js <==');
     assert.equal(createHeading('createHead.js'),'==> createHead.js <==');
   });
+
+  it('should create a head line when file name is empty', function(){
+    assert.equal(createHeading(''), '==>  <==');
+  });
+
+  it('should create a head line when no file name is given', function(){
+    assert.equal(createHeading(), '==> undefined <==');
+  });
+
 });
 
+
 describe('extractLines', function(){
+
   it('should give the numbers of line we want of a file', function(){
     let input = 'this\nis\na\nsample\nline';
     assert.equal(extractLines(input,1),'this');
     assert.equal(extractLines(input,3),'this\nis\na');
   });
+
+  it('should give the numbers of line when file size is less then user input', function(){
+    assert.equal(extractLines('sample\nline',12),'sample\nline');
+    assert.equal(extractLines('test\nline',15),'test\nline');
+  });
+
 });
 
+
 describe('extractBytes', function(){
+
   it('should give the number of bytes we want from a file', function(){
     let input = 'this\nis\na\nsample\nline';
     assert.equal(extractBytes(input,1),'t');
     assert.equal(extractBytes(input,5),'this\n');
   });
+
+  it('should give the number of bytes when file size is less then user input', function(){
+    assert.equal(extractBytes('line',5),'line');
+    assert.equal(extractBytes('sample\nfile',15),'sample\nfile');
+  });
+
 });
 
+
 describe('findInteger', function(){
+
   it('should return the integer from input', function(){
     assert.equal(findInteger(['-n1']),1);
     assert.equal(findInteger(['./head.js','-c5']),5);
   });
+
+  it('should return NaN when input have no integer', function(){
+    assert.equal(''+findInteger(['-n','-c']),'NaN');
+    assert.equal(''+findInteger(['./head.js','-c']),'NaN');
+  });
+
+  it('should return last integer when input have more then one integer', function(){
+    assert.equal(findInteger(['-n1','-c2']),2);
+    assert.equal(findInteger(['./head.js','8','-c5']),5);
+  });
+
 });
