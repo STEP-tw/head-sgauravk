3af1d5485e843a249b0df775469e0da081cc805a
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-07 15:12:28 +0530

    added test cases for findIllegalVal and extract error function

--- src/lib.js
+++ src/lib.js
@@ -1,78 +1,95 @@
-const createHeading = function(fileName){
-  return '==> ' + fileName + ' <==';
+const createHeading = function(fileName) {
+  return "==> " + fileName + " <==";
 };
 
-const extractLines = function(file,linesRequired){
-  let result = file.split('\n').slice(0,linesRequired);
-  return result.join('\n');
+const extractLines = function(file, linesRequired) {
+  let result = file.split("\n").slice(0, linesRequired);
+  return result.join("\n");
 };
 
-const extractBytes = function(file,BytesRequired){
-  let result = file.slice(0,BytesRequired);
+const extractBytes = function(file, BytesRequired) {
+  let result = file.slice(0, BytesRequired);
   return result;
 };
 
-const getHeadType = function(inputs){
-  let list = inputs.join('');
-  if(list.includes('-c')){
-    return 'extractBytes';
+const getHeadType = function(inputs) {
+  let list = inputs.join("");
+  if (list.includes("-c")) {
+    return "extractBytes";
   }
-  return 'extractLines';
+  return "extractLines";
 };
 
-const findInteger = function(input){
+const findInteger = function(input) {
   let index = 0;
-  let list = input.join('');
-  while(!parseInt(list) && index < input.join().length){
-    if (parseInt(list) == 0){ return 0; }
-    list = input.join('');
+  let list = input.join("");
+  while (!parseInt(list) && index < input.join().length) {
+    if (parseInt(list) == 0) {
+      return 0;
+    }
+    list = input.join("");
     index++;
     list = list.slice(index);
   }
-  if(!input.join('').includes('-c') && !input.join('').includes('-n')){
+  if (!input.join("").includes("-c") && !input.join("").includes("-n")) {
     return Math.abs(parseInt(list));
   }
   return parseInt(list);
 };
 
-const findIllegalVal = function(input){
-  let list = 'abdefghijklmopqrstuvwxyz0';
-  list = list.split('');
-  input = input.join('');
-  let result = '';
-  if (list.some(x=>input.includes(x))){
+const findIllegalVal = function(input) {
+  let list = "abdefghijklmopqrstuvwxyz0";
+  list = list.split("");
+  input = input.join("");
+  let result = "";
+  if (list.some(x => input.includes(x))) {
     result = input.slice(2);
   }
   return result;
 };
 
-const checkError = function(input){
-  let object = {'extractLines':'line count --','extractBytes':'byte count --'};
-  if (findIllegalVal(input) || findInteger(input) < 1){
-    return 'head: illegal '+object[getHeadType(input)]+' '+findIllegalVal(input)||findInteger(input);
+const extractError = function(input) {
+  let object = { extractLines: "line count --", extractBytes: "byte count --" };
+  if (findIllegalVal(input) || findInteger(input) < 1) {
+    return (
+      "head: illegal " +
+      object[getHeadType(input)] +
+      " " +
+      (findIllegalVal(input) || findInteger(input))
+    );
   }
 };
 
-const head = function (readFileSync,existsFileSync,inputs,filesList){
-  let delimiter = '';
-  if (checkError(inputs)){
-    console.log(checkError(inputs));
+const head = function(readFileSync, existsFileSync, inputs, filesList) {
+  let delimiter = "";
+  if (extractError(inputs)) {
+    console.log(extractError(inputs));
     return;
   }
-  for (let index=0; index<filesList.length; index++){
-    if (!existsFileSync(filesList[index])){
-      console.log('head:',filesList[index]+': No such file or directory');
+  for (let index = 0; index < filesList.length; index++) {
+    if (!existsFileSync(filesList[index])) {
+      console.log("head:", filesList[index] + ": No such file or directory");
       index++;
     }
-    if(index >= filesList.length){ return };
-    let content = readFileSync(filesList[index], 'utf8');
-    if (filesList.length > 1){
-      console.log(delimiter + createHeading(filesList[index]));
-      delimiter = '\n';
+    if (index >= filesList.length) {
+      return;
     }
-    console.log(eval(getHeadType(inputs))(content,findInteger(inputs) || 10));
+    let content = readFileSync(filesList[index], "utf8");
+    if (filesList.length > 1) {
+      console.log(delimiter + createHeading(filesList[index]));
+      delimiter = "\n";
+    }
+    console.log(eval(getHeadType(inputs))(content, findInteger(inputs) || 10));
   }
 };
- 
-module.exports = {createHeading, extractLines, extractBytes, getHeadType, findInteger, head, 
-findIllegalVal, checkError};
+
+module.exports = {
+  createHeading,
+  extractLines,
+  extractBytes,
+  getHeadType,
+  findInteger,
+  head,
+  findIllegalVal,
+  extractError
+};
--- test/lib_test.js
+++ test/lib_test.js
@@ -1,85 +1,118 @@
-const assert = require('assert');
-const {createHeading, extractLines, extractBytes, getHeadType, findInteger} = require('../src/lib.js');
+const assert = require("assert");
+const {
+  createHeading,
+  extractLines,
+  extractBytes,
+  getHeadType,
+  findInteger,
+  findIllegalVal,
+  extractError
+} = require("../src/lib.js");
 
-describe('createHeading', function(){
-
-  it('should create a head line using a file name', function(){
-    assert.equal(createHeading('lib.js'),'==> lib.js <==');
-    assert.equal(createHeading('createHead.js'),'==> createHead.js <==');
+describe("createHeading", function() {
+  it("should create a head line using a file name", function() {
+    assert.equal(createHeading("lib.js"), "==> lib.js <==");
+    assert.equal(createHeading("createHead.js"), "==> createHead.js <==");
   });
 
-  it('should create a head line when file name is empty', function(){
-    assert.equal(createHeading(''), '==>  <==');
+  it("should create a head line when file name is empty", function() {
+    assert.equal(createHeading(""), "==>  <==");
   });
 
-  it('should create a head line when no file name is given', function(){
-    assert.equal(createHeading(), '==> undefined <==');
+  it("should create a head line when no file name is given", function() {
+    assert.equal(createHeading(), "==> undefined <==");
   });
-
 });
 
-
-describe('extractLines', function(){
-
-  it('should give the numbers of line we want of a file', function(){
-    let input = 'this\nis\na\nsample\nline';
-    assert.equal(extractLines(input,1),'this');
-    assert.equal(extractLines(input,3),'this\nis\na');
+describe("extractLines", function() {
+  it("should give the numbers of line we want of a file", function() {
+    let input = "this\nis\na\nsample\nline";
+    assert.equal(extractLines(input, 1), "this");
+    assert.equal(extractLines(input, 3), "this\nis\na");
   });
 
-  it('should give the numbers of line when file size is less then user input', function(){
-    assert.equal(extractLines('sample\nline',12),'sample\nline');
-    assert.equal(extractLines('test\nline',15),'test\nline');
+  it("should give the numbers of line when file size is less then user input", function() {
+    assert.equal(extractLines("sample\nline", 12), "sample\nline");
+    assert.equal(extractLines("test\nline", 15), "test\nline");
   });
-
 });
 
-
-describe('extractBytes', function(){
-
-  it('should give the number of bytes we want from a file', function(){
-    let input = 'this\nis\na\nsample\nline';
-    assert.equal(extractBytes(input,1),'t');
-    assert.equal(extractBytes(input,5),'this\n');
+describe("extractBytes", function() {
+  it("should give the number of bytes we want from a file", function() {
+    let input = "this\nis\na\nsample\nline";
+    assert.equal(extractBytes(input, 1), "t");
+    assert.equal(extractBytes(input, 5), "this\n");
   });
 
-  it('should give the number of bytes when file size is less then user input', function(){
-    assert.equal(extractBytes('line',5),'line');
-    assert.equal(extractBytes('sample\nfile',15),'sample\nfile');
+  it("should give the number of bytes when file size is less then user input", function() {
+    assert.equal(extractBytes("line", 5), "line");
+    assert.equal(extractBytes("sample\nfile", 15), "sample\nfile");
   });
-
 });
 
-
-describe('getHeadType', function(){
-
-  it('should return the function according to the user input', function(){
-    assert.equal(getHeadType(['node','head.js','-n5']),'extractLines');
-    assert.equal(getHeadType(['node','head.js','-c3','file1']),'extractBytes');
+describe("getHeadType", function() {
+  it("should return the function according to the user input", function() {
+    assert.equal(getHeadType(["node", "head.js", "-n5"]), "extractLines");
+    assert.equal(
+      getHeadType(["node", "head.js", "-c3", "file1"]),
+      "extractBytes"
+    );
   });
 
-  it('should return extractLines function when no type is given', function(){
-    assert.equal(getHeadType(['node','head.js']),'extractLines');
+  it("should return extractLines function when no type is given", function() {
+    assert.equal(getHeadType(["node", "head.js"]), "extractLines");
   });
-
 });
 
-
-describe('findInteger', function(){
-
-  it('should return the integer from input', function(){
-    assert.equal(findInteger(['-n1']),1);
-    assert.equal(findInteger(['./head.js','-c5']),5);
+describe("findInteger", function() {
+  it("should return the integer from input", function() {
+    assert.equal(findInteger(["-n1"]), 1);
+    assert.equal(findInteger(["./head.js", "-c5"]), 5);
   });
 
-  it('should return NaN when input have no integer', function(){
-    assert.equal(''+findInteger(['-n','-c']),''+NaN);
-    assert.equal(''+findInteger(['./head.js','-c']),''+NaN);
+  it("should return NaN when input have no integer", function() {
+    assert.equal("" + findInteger(["-n", "-c"]), "" + NaN);
+    assert.equal("" + findInteger(["./head.js", "-c"]), "" + NaN);
   });
 
-  it('should return first integer when input have more then one integer', function(){
-    assert.equal(findInteger(['-n1','-c2']),1);
-    assert.equal(findInteger(['./head.js','c','12']),12);
+  it("should return first integer when input have more then one integer", function() {
+    assert.equal(findInteger(["-n1", "-c2"]), 1);
+    assert.equal(findInteger(["./head.js", "c", "12"]), 12);
+  });
+});
+
+describe("findIllegalVal", function() {
+  it("should return a empty string when no illegal count is given", function() {
+    assert.equal(findIllegalVal(["-n1"]), "");
+    assert.equal(findIllegalVal(["-n", "5"]), "");
   });
 
+  it("should return the illegal value present in the input", function() {
+    assert.equal(findIllegalVal(["-nr"]), "r");
+    assert.equal(findIllegalVal(["-n", "r1"]), "r1");
+    assert.equal(findIllegalVal(["-n", "-1r"]), "-1r");
+  });
+});
+
+describe("extractError", function() {
+  it("should return the undefined when no error present in input", function() {
+    assert.equal(extractError(["-n1"]), undefined);
+    assert.equal(extractError(["-n", "5"]), undefined);
+  });
+
+  it("should return the error message when we pass 0 in input", function() {
+    let expectedOutput = "head: illegal line count -- 0";
+    assert.equal(extractError(["-n0"]), expectedOutput);
+  });
+
+  it("should return the error message when a negative integer is passed in input", function() {
+    let expectedOutput = "head: illegal byte count -- -3";
+    assert.equal(extractError(["-c", "-3"]), expectedOutput);
+    assert.equal(extractError(["-c-3"]), expectedOutput);
+  });
+
+  it("should return the error message when illegal charecter is present in input", function() {
+    let expectedOutput = "head: illegal line count -- 1r";
+    assert.equal(extractError(["-n1r"]), expectedOutput);
+  });
 });
