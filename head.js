f84015a4738a84bd5e0cf793002b3cff95547055
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-10 12:21:33 +0530

    modify main to filtered file names

--- head.js
+++ head.js
@@ -3,7 +3,7 @@ const {head} = require('./src/lib.js');
 
 const main = function(){
   let inputs = process.argv.slice(2);
-  let filesList = inputs.filter(file => file.includes('.'));
+  let filesList = inputs.filter(file => !file.startsWith('-') && !file.match(/^\d/));
   inputs = inputs.slice(0, inputs.length-filesList.length);
   console.log(head(fs.readFileSync,fs.existsSync,inputs,filesList));
 };
