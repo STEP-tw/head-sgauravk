e5a188e1dd7f8b670fca912f1d27a155d26dfd17
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-13 15:39:32 +0530

    refactor tail.js to handle 0 as a legel count

--- tail.js
+++ tail.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
-const {tail} = require('./src/lib.js');
+const {tail, findInteger} = require('./src/lib.js');
 
 const main = function(){
   let inputs = process.argv.slice(2);
   let filesList = inputs.filter(file => !file.startsWith('-') && !file.match(/^\d/));
   inputs = inputs.slice(0, inputs.length-filesList.length);
+  if (findInteger(inputs) == '0'){
+    process.exit(); }
   console.log(tail(fs.readFileSync,fs.existsSync,inputs,filesList));
 };
 
