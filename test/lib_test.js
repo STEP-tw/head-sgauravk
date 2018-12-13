31b6cf733aa38b22f613020baab0e928741f7c50
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-13 21:10:18 +0530

    add more test case for head function for multiple file

--- test/lib_test.js
+++ test/lib_test.js
@@ -11,57 +11,50 @@ const {
   tail
 } = require("../src/lib.js");
 
-const readFile = function(file){
-  return file;
-};
 
-let existsFile = function(file){
-  return true;
-}
-
-describe("createHeading", function() {
-  it("should create a head line using a file name", function() {
+describe("createHeading", function(){
+  it("should create a head line using a file name", function(){
     assert.equal(createHeading("lib.js"), "==> lib.js <==");
     assert.equal(createHeading("createHead.js"), "==> createHead.js <==");
   });
 
-  it("should create a head line when file name is empty", function() {
+  it("should create a head line when file name is empty", function(){
     assert.equal(createHeading(""), "==>  <==");
   });
 
-  it("should create a head line when no file name is given", function() {
+  it("should create a head line when no file name is given", function(){
     assert.equal(createHeading(), "==> undefined <==");
   });
 });
 
-describe("extractLines", function() {
-  it("should give the numbers of line we want of a file", function() {
+describe("extractLines", function(){
+  it("should give the numbers of line we want of a file", function(){
     let input = "this\nis\na\nsample\nline";
     assert.equal(extractLines(0,input, 1), "this");
     assert.equal(extractLines(0,input, 3), "this\nis\na");
   });
 
-  it("should give the numbers of line when file size is less then user input", function() {
+  it("should give the numbers of line when file size is less then user input", function(){
     assert.equal(extractLines(0,"sample\nline", 12), "sample\nline");
     assert.equal(extractLines(0,"test\nline", 15), "test\nline");
   });
 });
 
-describe("extractBytes", function() {
-  it("should give the number of bytes we want from a file", function() {
+describe("extractBytes", function(){
+  it("should give the number of bytes we want from a file", function(){
     let input = "this\nis\na\nsample\nline";
     assert.equal(extractBytes(0,input, 1), "t");
     assert.equal(extractBytes(0,input, 5), "this\n");
   });
 
-  it("should give the number of bytes when file size is less then user input", function() {
+  it("should give the number of bytes when file size is less then user input", function(){
     assert.equal(extractBytes(0,"line", 5), "line");
     assert.equal(extractBytes(0,"sample\nfile", 15), "sample\nfile");
   });
 });
 
-describe("getHeadType", function() {
-  it("should return the function according to the user input", function() {
+describe("getHeadType", function(){
+  it("should return the function according to the user input", function(){
     assert.equal(getHeadType(["node", "head.js", "-n5"]), "extractLines");
     assert.equal(
       getHeadType(["node", "head.js", "-c3", "file1"]),
@@ -69,59 +62,59 @@ describe("getHeadType", function() {
     );
   });
 
-  it("should return extractLines function when no type is given", function() {
+  it("should return extractLines function when no type is given", function(){
     assert.equal(getHeadType(["node", "head.js"]), "extractLines");
   });
 });
 
-describe("findInteger", function() {
-  it("should return the integer from input", function() {
+describe("findInteger", function(){
+  it("should return the integer from input", function(){
     assert.equal(findInteger(["-n1"]), 1);
     assert.equal(findInteger(["./head.js", "-c5"]), 5);
   });
 
-  it("should return NaN when input have no integer", function() {
+  it("should return NaN when input have no integer", function(){
     assert.equal("" + findInteger(["-n", "-c"]), "" + NaN);
     assert.equal("" + findInteger(["./head.js", "-c"]), "" + NaN);
   });
 
-  it("should return first integer when input have more then one integer", function() {
+  it("should return first integer when input have more then one integer", function(){
     assert.equal(findInteger(["-n1", "-c2"]), 1);
     assert.equal(findInteger(["./head.js", "c", "12"]), 12);
   });
 });
 
-describe("findIllegalVal", function() {
-  it("should return a empty string when no illegal count is given", function() {
+describe("findIllegalVal", function(){
+  it("should return a empty string when no illegal count is given", function(){
     assert.equal(findIllegalVal(["-n1"]), "");
     assert.equal(findIllegalVal(["-n", "5"]), "");
   });
 
-  it("should return the illegal value present in the input", function() {
+  it("should return the illegal value present in the input", function(){
     assert.equal(findIllegalVal(["-nr"]), "r");
     assert.equal(findIllegalVal(["-n", "r1"]), "r1");
     assert.equal(findIllegalVal(["-n", "-1r"]), "-1r");
   });
 });
 
-describe("extractError", function() {
-  it("should return the undefined when no error present in input", function() {
+describe("extractError", function(){
+  it("should return the undefined when no error present in input", function(){
     assert.equal(extractError(["-n1"],'head'), undefined);
     assert.equal(extractError(["-n", "5"],'head'), undefined);
   });
 
-  it("should return the error message when we pass 0 in input", function() {
+  it("should return the error message when we pass 0 in input", function(){
     let expectedOutput = "head: illegal line count -- 0";
     assert.equal(extractError(["-n0"],'head'), expectedOutput);
   });
 
-  it("should return the error message when a negative integer is passed in input", function() {
+  it("should return the error message when a negative integer is passed in input", function(){
     let expectedOutput = "head: illegal byte count -- -3";
     assert.equal(extractError(["-c", "-3"],'head'), expectedOutput);
     assert.equal(extractError(["-c-3"],'head'), expectedOutput);
   });
 
-  it("should return the error message when illegal charecter is present in input", function() {
+  it("should return the error message when illegal charecter is present in input", function(){
     let expectedOutput = "head: illegal line count -- 1r";
     assert.equal(extractError(["-n1r"],'head'), expectedOutput);
   });
@@ -130,6 +123,15 @@ describe("extractError", function() {
 
 
 describe('HEAD: for single file', function(){
+
+  let existsFile = function(file){
+    return true;
+  }
+
+  let readFile = function(file){
+    return file;
+  };
+
   let file = '1\n2\n3\n4\n5\n6\n7\n8\n9\n10';
   let randomText = 'gaurav\nis\na\ngood\nboy';
 
@@ -142,27 +144,84 @@ describe('HEAD: for single file', function(){
   });
 
   it("should return the first 10 lines of file when count is not specified", function (){
-    assert.deepEqual(head(readFile,existsFile,[],[file]),file)
+    assert.equal(head(readFile,existsFile,[],[file]),file)
   });
-  
-  it("should return the given number of lines when only count is given", function () {
-    assert.deepEqual(head(readFile,existsFile,['-3'],[file]), "1\n2\n3");
+
+  it("should return the given number of lines when only count is given", function(){
+    assert.equal(head(readFile,existsFile,['-3'],[file]), "1\n2\n3");
   });
-  
-  it("should return the given number of lines when count and option is given without spaces", function () {
-    assert.deepEqual(head(readFile,existsFile,["-n2"], [file]), "1\n2");
+
+  it("should return the given number of lines when count and option is given without spaces", function(){
+    assert.equal(head(readFile,existsFile,["-n2"], [file]), "1\n2");
   });
-  
-  it("should return the given number of lines when count and option is given with spaces", function () {
-    assert.deepEqual(head(readFile,existsFile,["-n", "1"],[file]), '1');
+
+  it("should return the given number of lines when count and option is given with spaces", function(){
+    assert.equal(head(readFile,existsFile,["-n", "1"],[file]), '1');
   });
-  
-  it("should return the given number of characters when count is given with spaces", function () {
-    assert.deepEqual(head(readFile,existsFile,["-c", "3"], [randomText]), "gau");
+
+  it("should return the given number of characters when count is given with spaces", function(){
+    assert.equal(head(readFile,existsFile,["-c", "3"], [randomText]), "gau");
   });
-  
-  it("should return the given number of characters when count is given without spaces", function () {
-    assert.deepEqual(head(readFile,existsFile,["-c7"], [randomText]), "gaurav\n");
+
+  it("should return the given number of characters when count is given without spaces", function(){
+    assert.equal(head(readFile,existsFile,["-c7"], [randomText]), "gaurav\n");
   });
-  
+
 });
+
+
+describe("HEAD: for multiple file", function(){
+
+  let file = '1\n2\n3\n4\n5\n6\n7\n8\n9\n10';
+  let randomText = 'gaurav\nis\na\ngood\nboy';
+
+  let existsFile = function(file){
+    return true;
+  }
+
+  let readFile = function(file){
+    return "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+  };
+
+  let readRandomText = function(randomText){
+    return 'gaurav\nis\na\ngood\nboy';
+  }
+
+  it("should return the first 10 lines of file when count is not specified", function(){
+    let expectedOutput = "==> file <==\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+    expectedOutput += '\n\n' + expectedOutput;
+    assert.equal(head(readFile,existsFile,[],['file','file']), expectedOutput);
+  });
+
+  it("should return the given number of lines when only count is given", function(){
+    let expectedOutput = "==> file <==\n1\n2\n3"
+    expectedOutput+= '\n\n' + expectedOutput;
+    assert.equal(head(readFile,existsFile,['-3'], ["file", "file"]), expectedOutput);
+  });
+
+  it("should return the given number of lines when count and option is given without spaces", function(){
+    let expectedOutput = "==> file <==\n1\n2"
+    expectedOutput+= '\n\n' + expectedOutput;
+    assert.equal(head(readFile,existsFile,["-n2"], ['file','file']), expectedOutput);
+  });
+
+  it("should return the given number of lines when count and option is given with spaces", function(){
+    let expectedOutput = "==> file <==\n1\n2\n3"
+    expectedOutput+= '\n\n' + expectedOutput;
+    assert.equal(head(readFile,existsFile,["-n", "3"],["file", "file"]), expectedOutput);
+  });
+
+  it("should return the given number of characters when count is given with spaces", function(){
+    let expectedOutput = "==> randomText <==\ngau";
+    expectedOutput += '\n\n' + expectedOutput;
+    assert.equal(head(readRandomText,existsFile,["-c","3"],["randomText","randomText"]), expectedOutput);
+  });
+
+  it("should return the given number of characters when count is given without spaces", function(){
+    let expectedOutput = "==> randomText <==\ngaurav\n"
+    expectedOutput+= '\n\n' + expectedOutput;
+    assert.equal(head(readRandomText,existsFile,["-c7"],["randomText", "randomText"]), expectedOutput);
+  });
+
+});
+
