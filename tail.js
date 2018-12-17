07cee35a62ccf4145eecde83de0f828d91dbf7c1
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-18 00:37:22 +0530

    apply prettier on tail.js

--- tail.js
+++ tail.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
-const {tail, extractCount} = require('./src/lib.js');
+const fs = require("fs");
+const { tail, extractCount } = require("./src/lib.js");
 
-const main = function(){
+const main = function() {
   let inputs = process.argv.slice(2);
-  let filesList = inputs.filter(file => !file.startsWith('-') && !file.match(/^\d/));
-  inputs = inputs.slice(0, inputs.length-filesList.length);
-  if (extractCount(inputs) == '0') { process.exit(); }
-  console.log(tail(fs.readFileSync,fs.existsSync,inputs,filesList));
+  let filesList = inputs.filter(
+    file => !file.startsWith("-") && !file.match(/^\d/)
+  );
+  inputs = inputs.slice(0, inputs.length - filesList.length);
+  if (extractCount(inputs) == "0") {
+    process.exit();
+  }
+  console.log(tail(fs.readFileSync, fs.existsSync, inputs, filesList));
 };
 
 main();
-
