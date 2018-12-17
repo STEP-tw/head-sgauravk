d6ee7509cdfbe3a0396d3dc09a733177f746d9d9
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-18 00:38:17 +0530

    modify tail.js

--- tail.js
+++ tail.js
@@ -3,13 +3,9 @@ const { tail, extractCount } = require("./src/lib.js");
 
 const main = function() {
   let inputs = process.argv.slice(2);
-  let filesList = inputs.filter(
-    file => !file.startsWith("-") && !file.match(/^\d/)
-  );
+  let filesList = inputs.filter(file => !file.startsWith("-") && !file.match(/^\d/));
   inputs = inputs.slice(0, inputs.length - filesList.length);
-  if (extractCount(inputs) == "0") {
-    process.exit();
-  }
+  if (extractCount(inputs) == "0") process.exit();
   console.log(tail(fs.readFileSync, fs.existsSync, inputs, filesList));
 };
 
