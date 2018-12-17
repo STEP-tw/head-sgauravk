97102bf82412573b97e8da2efb1f37fb51333d56
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-18 00:31:22 +0530

    use a separate describe block for tail

--- test/lib_test.js
+++ test/lib_test.js
@@ -285,173 +285,175 @@ describe("HEAD", function(){
   });
 });
 
-describe("TAIL: for single file", function() {
-  let existsFile = function(file) {
-    return [file, randomText].includes(file);
-  };
+describe("TAIL", function(){
+  describe("TAIL: for single file", function() {
+    let existsFile = function(file) {
+      return [file, randomText].includes(file);
+    };
 
-  let readFile = function(file) {
-    return file;
-  };
+    let readFile = function(file) {
+      return file;
+    };
 
-  let file = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
-  let randomText = "gaurav\nis\na\ngood\nboy";
+    let file = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+    let randomText = "gaurav\nis\na\ngood\nboy";
 
-  it("should return the first line of file", function() {
-    assert.equal(tail(readFile, existsFile, ["-n1"], [file]), "10");
+    it("should return the first line of file", function() {
+      assert.equal(tail(readFile, existsFile, ["-n1"], [file]), "10");
+    });
+
+    it("should return the two lines of file", function() {
+      assert.equal(tail(readFile, existsFile, ["-n2"], [file]), "9\n10");
+    });
+
+    it("should return the first 10 lines of file when count is not specified", function() {
+      assert.equal(tail(readFile, existsFile, [], [file]), file);
+    });
+
+    it("should return the given number of lines when only count is given", function() {
+      assert.equal(tail(readFile, existsFile, ["-3"], [file]), "8\n9\n10");
+    });
+
+    it("should return the given number of lines when count and option is given without spaces", function() {
+      assert.equal(tail(readFile, existsFile, ["-n2"], [file]), "9\n10");
+    });
+
+    it("should return the given number of lines when count and option is given with spaces", function() {
+      assert.equal(tail(readFile, existsFile, ["-n", "1"], [file]), "10");
+    });
+
+    it("should return the given number of characters when count is given with spaces", function() {
+      assert.equal(tail(readFile, existsFile, ["-c", "3"], [randomText]), "boy");
+    });
+
+    it("should return the given number of characters when count is given without spaces", function() {
+      assert.equal(tail(readFile, existsFile, ["-c7"], [randomText]), "ood\nboy");
+    });
+
+    it("should return the whole file when count is greter file size", function() {
+      assert.equal(tail(readFile, existsFile, ["-n10000"], [file]), file);
+    });
   });
 
-  it("should return the two lines of file", function() {
-    assert.equal(tail(readFile, existsFile, ["-n2"], [file]), "9\n10");
+  describe("TAIL: for multiple file", function() {
+    let file = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+    let randomText = "gaurav\nis\na\ngood\nboy";
+
+    let existsFile = function(file) {
+      return [file, randomText].includes(file);
+    };
+
+    let readFile = function(file) {
+      return "10\n9\n8\n7\n6\n5\n4\n3\n2\n1";
+    };
+
+    let readRandomText = function(randomText) {
+      return "gaurav\nis\na\ngood\nboy";
+    };
+
+    it("should return the first 10 lines of file when count is not specified", function() {
+      let expectedOutput = "==> file <==\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1";
+      expectedOutput += "\n\n" + expectedOutput;
+      assert.equal(
+        tail(readFile, existsFile, [], ["file", "file"]),expectedOutput);
+    });
+
+    it("should return the given number of lines when only count is given", function() {
+      let expectedOutput = "==> file <==\n3\n2\n1";
+      expectedOutput += "\n\n" + expectedOutput;
+      assert.equal(
+        tail(readFile, existsFile, ["-3"], ["file", "file"]),expectedOutput);
+    });
+
+    it("should return the given number of lines when count and option is given without spaces", function() {
+      let expectedOutput = "==> file <==\n2\n1";
+      expectedOutput += "\n\n" + expectedOutput;
+      assert.equal(
+        tail(readFile, existsFile, ["-n2"], ["file", "file"]),expectedOutput);
+    });
+
+    it("should return the given number of lines when count and option is given with spaces", function() {
+      let expectedOutput = "==> file <==\n3\n2\n1";
+      expectedOutput += "\n\n" + expectedOutput;
+      assert.equal(
+        tail(readFile, existsFile, ["-n", "3"], ["file", "file"]),expectedOutput);
+    });
+
+    it("should return the given number of characters when count is given with spaces", function() {
+      let expectedOutput = "==> randomText <==\nboy";
+      expectedOutput += "\n\n" + expectedOutput;
+      assert.equal(
+        tail(readRandomText,existsFile,["-c", "3"],["randomText", "randomText"]),expectedOutput);
+    });
+
+    it("should return the given number of characters when count is given without spaces", function() {
+      let expectedOutput = "==> randomText <==\nood\nboy";
+      expectedOutput += "\n\n" + expectedOutput;
+      assert.equal(
+        tail(readRandomText, existsFile, ["-c7"], ["randomText", "randomText"]),expectedOutput);
+    });
   });
 
-  it("should return the first 10 lines of file when count is not specified", function() {
-    assert.equal(tail(readFile, existsFile, [], [file]), file);
-  });
+  describe("TAIL: errors handling", function() {
+    let file = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+    let randomText = "gaurav\nis\na\ngood\nboy";
 
-  it("should return the given number of lines when only count is given", function() {
-    assert.equal(tail(readFile, existsFile, ["-3"], [file]), "8\n9\n10");
-  });
+    let existsFile = function(file) {
+      return ["file", "randomText"].includes(file);
+    };
 
-  it("should return the given number of lines when count and option is given without spaces", function() {
-    assert.equal(tail(readFile, existsFile, ["-n2"], [file]), "9\n10");
-  });
+    let readFile = function(file) {
+      return "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
+    };
 
-  it("should return the given number of lines when count and option is given with spaces", function() {
-    assert.equal(tail(readFile, existsFile, ["-n", "1"], [file]), "10");
-  });
+    let readRandomText = function(randomText) {
+      return "gaurav\nis\na\ngood\nboy";
+    };
 
-  it("should return the given number of characters when count is given with spaces", function() {
-    assert.equal(tail(readFile, existsFile, ["-c", "3"], [randomText]), "boy");
-  });
+    it("should return the error message when given number of line is 0", function() {
+      let expectedOutput = "tail: illegal offset -- 0";
+      assert.equal(tail(readFile, existsFile, ["-n0"], [file]), expectedOutput);
+    });
 
-  it("should return the given number of characters when count is given without spaces", function() {
-    assert.equal(tail(readFile, existsFile, ["-c7"], [randomText]), "ood\nboy");
-  });
+    it("should return the error message when  is count is 0 without -c or -n", function() {
+      let expectedOutput = "tail: illegal offset -- 0";
+      assert.equal(
+        tail(readFile, existsFile, ["-0"], ["file", "randomText"]),expectedOutput);
+    });
 
-  it("should return the whole file when count is greter file size", function() {
-    assert.equal(tail(readFile, existsFile, ["-n10000"], [file]), file);
-  });
-});
+    it("should return the error message when  is count is invalid with -c or -n", function() {
+      let expectedOutput = "tail: illegal offset -- -12";
+      assert.equal(
+        tail(readFile, existsFile, ["-n-12"], ["file", "randomText"]),expectedOutput);
+    });
 
-describe("TAIL: for multiple file", function() {
-  let file = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
-  let randomText = "gaurav\nis\na\ngood\nboy";
+    it("should return the error message when  file is not present in the directory", function() {
+      let expectedOutput =
+        "tail: README.mdafs: No such file or directory\n\n==> file <==\n8\n9\n10";
+      assert.equal(
+        tail(readFile, existsFile, ["-n3"], ["README.mdafs", "file"]),expectedOutput);
+    });
 
-  let existsFile = function(file) {
-    return [file, randomText].includes(file);
-  };
+    it("should return the error message when given file is not present in directory", function() {
+      let expectedOutput = "tail: README.mdafs: No such file or directory";
+      assert.equal(
+        tail(readFile, existsFile, ["-n3"], ["README.mdafs"]),expectedOutput);
+    });
 
-  let readFile = function(file) {
-    return "10\n9\n8\n7\n6\n5\n4\n3\n2\n1";
-  };
+    it("should return the whole file when count is greter file size and other is invalid", function() {
+      let expectedOutput =
+        "tail: README.mdsn: No such file or directory\n\n==> file <==\n" + file;
+      assert.equal(
+        tail(readFile, existsFile, ["-n10000"], ["README.mdsn", "file"]),expectedOutput);
+    });
 
-  let readRandomText = function(randomText) {
-    return "gaurav\nis\na\ngood\nboy";
-  };
-
-  it("should return the first 10 lines of file when count is not specified", function() {
-    let expectedOutput = "==> file <==\n10\n9\n8\n7\n6\n5\n4\n3\n2\n1";
-    expectedOutput += "\n\n" + expectedOutput;
-    assert.equal(
-      tail(readFile, existsFile, [], ["file", "file"]),expectedOutput);
-  });
-
-  it("should return the given number of lines when only count is given", function() {
-    let expectedOutput = "==> file <==\n3\n2\n1";
-    expectedOutput += "\n\n" + expectedOutput;
-    assert.equal(
-      tail(readFile, existsFile, ["-3"], ["file", "file"]),expectedOutput);
-  });
-
-  it("should return the given number of lines when count and option is given without spaces", function() {
-    let expectedOutput = "==> file <==\n2\n1";
-    expectedOutput += "\n\n" + expectedOutput;
-    assert.equal(
-      tail(readFile, existsFile, ["-n2"], ["file", "file"]),expectedOutput);
-  });
-
-  it("should return the given number of lines when count and option is given with spaces", function() {
-    let expectedOutput = "==> file <==\n3\n2\n1";
-    expectedOutput += "\n\n" + expectedOutput;
-    assert.equal(
-      tail(readFile, existsFile, ["-n", "3"], ["file", "file"]),expectedOutput);
-  });
-
-  it("should return the given number of characters when count is given with spaces", function() {
-    let expectedOutput = "==> randomText <==\nboy";
-    expectedOutput += "\n\n" + expectedOutput;
-    assert.equal(
-      tail(readRandomText,existsFile,["-c", "3"],["randomText", "randomText"]),expectedOutput);
-  });
-
-  it("should return the given number of characters when count is given without spaces", function() {
-    let expectedOutput = "==> randomText <==\nood\nboy";
-    expectedOutput += "\n\n" + expectedOutput;
-    assert.equal(
-      tail(readRandomText, existsFile, ["-c7"], ["randomText", "randomText"]),expectedOutput);
-  });
-});
-
-describe("TAIL: errors handling", function() {
-  let file = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
-  let randomText = "gaurav\nis\na\ngood\nboy";
-
-  let existsFile = function(file) {
-    return ["file", "randomText"].includes(file);
-  };
-
-  let readFile = function(file) {
-    return "1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
-  };
-
-  let readRandomText = function(randomText) {
-    return "gaurav\nis\na\ngood\nboy";
-  };
-
-  it("should return the error message when given number of line is 0", function() {
-    let expectedOutput = "tail: illegal offset -- 0";
-    assert.equal(tail(readFile, existsFile, ["-n0"], [file]), expectedOutput);
-  });
-
-  it("should return the error message when  is count is 0 without -c or -n", function() {
-    let expectedOutput = "tail: illegal offset -- 0";
-    assert.equal(
-      tail(readFile, existsFile, ["-0"], ["file", "randomText"]),expectedOutput);
-  });
-
-  it("should return the error message when  is count is invalid with -c or -n", function() {
-    let expectedOutput = "tail: illegal offset -- -12";
-    assert.equal(
-      tail(readFile, existsFile, ["-n-12"], ["file", "randomText"]),expectedOutput);
-  });
-
-  it("should return the error message when  file is not present in the directory", function() {
-    let expectedOutput =
-      "tail: README.mdafs: No such file or directory\n\n==> file <==\n8\n9\n10";
-    assert.equal(
-      tail(readFile, existsFile, ["-n3"], ["README.mdafs", "file"]),expectedOutput);
-  });
-
-  it("should return the error message when given file is not present in directory", function() {
-    let expectedOutput = "tail: README.mdafs: No such file or directory";
-    assert.equal(
-      tail(readFile, existsFile, ["-n3"], ["README.mdafs"]),expectedOutput);
-  });
-
-  it("should return the whole file when count is greter file size and other is invalid", function() {
-    let expectedOutput =
-      "tail: README.mdsn: No such file or directory\n\n==> file <==\n" + file;
-    assert.equal(
-      tail(readFile, existsFile, ["-n10000"], ["README.mdsn", "file"]),expectedOutput);
-  });
-
-  it("should return the error message when -n or -c and then alphanumeric combination is given", function() {
-    let expectedOutput = "tail: illegal offset -- u922";
-    assert.equal(
-      tail(readFile, existsFile, ["-nu922"], ["README.mdafs", "file"]),expectedOutput);
-    expectedOutput = "tail: illegal offset -- u922";
-    assert.equal(
-      tail(readFile, existsFile, ["-cu922"], ["README.mdafs", "randomText"]),expectedOutput);
+    it("should return the error message when -n or -c and then alphanumeric combination is given", function() {
+      let expectedOutput = "tail: illegal offset -- u922";
+      assert.equal(
+        tail(readFile, existsFile, ["-nu922"], ["README.mdafs", "file"]),expectedOutput);
+      expectedOutput = "tail: illegal offset -- u922";
+      assert.equal(
+        tail(readFile, existsFile, ["-cu922"], ["README.mdafs", "randomText"]),expectedOutput);
+    });
   });
 });
