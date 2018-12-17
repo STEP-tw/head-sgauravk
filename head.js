474235a28da539085ff38eeaff0d3f21558405d6
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-18 00:36:31 +0530

    apply prettier on head.js

--- head.js
+++ head.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-const {head} = require('./src/lib.js');
+const fs = require("fs");
+const { head } = require("./src/lib.js");
 
-const main = function(){
+const main = function() {
   let inputs = process.argv.slice(2);
-  let filesList = inputs.filter(file => !file.startsWith('-') && !file.match(/^\d/));
-  inputs = inputs.slice(0, inputs.length-filesList.length);
-  console.log(head(fs.readFileSync,fs.existsSync,inputs,filesList));
+  let filesList = inputs.filter(file => !file.startsWith("-") && !file.match(/^\d/));
+  inputs = inputs.slice(0, inputs.length - filesList.length);
+  console.log(head(fs.readFileSync, fs.existsSync, inputs, filesList));
 };
 
 main();
