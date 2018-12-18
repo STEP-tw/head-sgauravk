91328441e4bfa0059569d1726c868188af2a9152
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-18 15:53:32 +0530

    modify tail.js and used reqired parseUserInput function in tail.js

--- tail.js
+++ tail.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
-const { getFinalOutput, extractCount } = require("./src/lib.js");
+const { getFinalOutput } = require("./src/lib.js");
+const { extractCount } = require("./src/util.js");
+const { parseUserInput } = require("./src/parseInput");
 
 const main = function() {
   let inputs = process.argv.slice(2);
-  let filesList = inputs.filter(file => !file.startsWith("-") && !file.match(/^\d/));
-  inputs = inputs.slice(0, inputs.length - filesList.length);
-  if (extractCount(inputs) == "0") process.exit();
-  console.log(getFinalOutput(fs.readFileSync, fs.existsSync, inputs, filesList, 'tail'));
+  const { input, filesList } = parseUserInput(inputs);
+   if (extractCount(input) == "0") process.exit();
+  console.log(getFinalOutput(fs.readFileSync, fs.existsSync, input, filesList, 'tail'));
 };
 
 main();
