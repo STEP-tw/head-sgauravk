e3c580f7536d3c33bc830a2aeabfe3057acd2acf
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-18 10:50:38 +0530

    modify tail.js and renamed the function name

--- tail.js
+++ tail.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
-const { tail, extractCount } = require("./src/lib.js");
+const { getContent, extractCount } = require("./src/lib.js");
 
 const main = function() {
   let inputs = process.argv.slice(2);
   let filesList = inputs.filter(file => !file.startsWith("-") && !file.match(/^\d/));
   inputs = inputs.slice(0, inputs.length - filesList.length);
   if (extractCount(inputs) == "0") process.exit();
-  console.log(tail(fs.readFileSync, fs.existsSync, inputs, filesList));
+  console.log(getContent(fs.readFileSync, fs.existsSync, inputs, filesList, 'tail'));
 };
 
 main();
