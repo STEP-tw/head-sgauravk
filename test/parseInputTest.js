7aae36406ee69f5aaa6b7ce8c0833a580af7c6e0
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-19 12:12:49 +0530

    added one more test case for parseInput

--- test/parseInputTest.js
+++ test/parseInputTest.js
@@ -1,10 +1,17 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const { parseUserInput } = require('../src/parseInput');
+const { parseUserInput } = require("../src/parseInput");
 
-describe('parseUserInput', function () {
-    it('it should return input after parsing the user input', function () {
-        let expectedOutput = {input:['-n1'], filesList:['readme.md']};
-        assert.deepEqual(parseUserInput(['-n1','readme.md']), expectedOutput);
-    });
+describe("parseUserInput", function() {
+  it("it should return input after parsing the user input", function() {
+    let expectedOutput = { input: ["-n1"], filesList: ["readme.md"] };
+    assert.deepEqual(parseUserInput(["-n1", "readme.md"]), expectedOutput);
+  });
+
+  it("shold seperate the input when files is greter then 1", function(){
+    let userInput = ['-c','10','readme.md','ABC.js'];
+    let expectedOutput = { input: ["-c",'10'], filesList: ["readme.md","ABC.js"] };
+    assert.deepEqual(parseUserInput(userInput), expectedOutput);
+  });
+
 });
