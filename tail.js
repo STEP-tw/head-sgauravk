7e61e5ba7a13464d6570d7e1fe8e34bf34d2d813
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-19 12:33:48 +0530

    indent tail.js

--- tail.js
+++ tail.js
@@ -3,10 +3,10 @@ const { getFinalOutput } = require("./src/lib.js");
 const { extractCount } = require("./src/util.js");
 const { parseUserInput } = require("./src/parseInput");
 
-const main = function() {
+const main = function () {
   let inputs = process.argv.slice(2);
   const { input, filesList } = parseUserInput(inputs);
-   if (extractCount(input) == "0") process.exit();
+  if (extractCount(input) == "0") process.exit();
   console.log(getFinalOutput(fs.readFileSync, fs.existsSync, input, filesList, 'tail'));
 };
 
