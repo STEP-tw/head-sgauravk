b27401dea8c5b03b224f893ad4a8d9c050f25f99
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-19 12:30:49 +0530

    indent parseInputTest.js

--- test/parseInputTest.js
+++ test/parseInputTest.js
@@ -2,19 +2,19 @@ const assert = require("assert");
 
 const { parseUserInput } = require("../src/parseInput");
 
-describe("parseUserInput", function() {
-  it("it should return input after parsing the user input", function() {
+describe("parseUserInput", function () {
+  it("it should return input after parsing the user input", function () {
     let expectedOutput = { input: ["-n1"], filesList: ["readme.md"] };
     assert.deepEqual(parseUserInput(["-n1", "readme.md"]), expectedOutput);
   });
 
-  it("shold seperate the input when files is greter then 1", function(){
-    let userInput = ['-c','10','readme.md','ABC.js'];
-    let expectedOutput = { input: ["-c",'10'], filesList: ["readme.md","ABC.js"] };
+  it("shold seperate the input when files is greter then 1", function () {
+    let userInput = ['-c', '10', 'readme.md', 'ABC.js'];
+    let expectedOutput = { input: ["-c", '10'], filesList: ["readme.md", "ABC.js"] };
     assert.deepEqual(parseUserInput(userInput), expectedOutput);
   });
 
-  it('should return empty array of input when only file names is given', function(){
+  it('should return empty array of input when only file names is given', function () {
     let expectedOutput = { input: [], filesList: ["readme.md"] };
     assert.deepEqual(parseUserInput(['readme.md']), expectedOutput);
   });
