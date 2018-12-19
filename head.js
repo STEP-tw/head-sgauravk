2f5f70cc7f6e1df59383fa5e75b23b4db2ccf3e6
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2018-12-19 12:32:01 +0530

    indent head.js

--- head.js
+++ head.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const { getFinalOutput } = require("./src/lib.js");
 const { parseUserInput } = require("./src/parseInput");
 
-const main = function() {
+const main = function () {
   let inputs = process.argv.slice(2);
   const { input, filesList } = parseUserInput(inputs);
-  console.log(getFinalOutput( fs.readFileSync, fs.existsSync, input, filesList, 'head'));
+  console.log(getFinalOutput(fs.readFileSync, fs.existsSync, input, filesList, 'head'));
 };
 
 main();
